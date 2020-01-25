b94d25094bf4ceda6b10ed0d48fee0549808bc5c
Lucas Godoy <lcsdegodoy@outlook.com>
2020-01-25 15:06:09 -0300

    feat: send message to SQS

--- functions/create_log_entry.ts
+++ functions/create_log_entry.ts
@@ -1,12 +1,38 @@
 'use strict'
 
+import { SQS } from 'aws-sdk'
+
+const queue = new SQS()
+
 module.exports.create = (
   event: any,
   context: any,
   callback: (arg0: any, arg1: any) => void
 ) => {
-  const response = {
-    statusCode: 200
+  const params: SQS.SendMessageParams = {
+    MessageBody: event.body,
+    QueueUrl: process.env.QUEUE_URL
   }
-  callback(null, response)
+
+  queue.sendMessage(params, function (err, data) {
+    if (err) {
+      console.log(err)
+
+      const response = {
+        statusCode: 500,
+        body: JSON.stringify({
+          status: false
+        })
+      }
+      callback(null, response)
+    } else {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          status: true
+        })
+      }
+      callback(null, response)
+    }
+  })
 }
